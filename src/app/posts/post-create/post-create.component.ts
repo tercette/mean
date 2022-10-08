1e6483e0f01d9eabac352428fcbe23f06b64d8e5
tercette <leandro.tercette@gmail.com>
2022-10-08 13:01:19 -0300

    express framework installed

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -20,14 +20,7 @@ export class PostCreateComponent {
     if(form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title ,
-      content: form.value.content
-    }
-    this.postService.addPost(form.value.title, form.value.content)
-
-    
-
-
+        this.postService.addPost(form.value.title, form.value.content)
+        form.resetForm()
   }
 }
