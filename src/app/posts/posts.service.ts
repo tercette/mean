ebaaba7f039dd2583ee391afdbf559b27346d103
tercette <leandro.tercette@gmail.com>
2022-10-09 14:52:55 -0300

    Updating posts on the server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,7 +38,9 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {...this.posts.find(p => p.id ===id)};
+    return this.http.get<{ _id: string, title: string, content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
   }
 
   addPost(title: string, content: string) {
@@ -56,16 +58,22 @@ export class PostsService {
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
     this.http
-    .put("http://localhost:3000/api/posts/" + id, post)
-    .subscribe(response => console.log(response))
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      })
   }
 
-  deletePost(postId: string){
+  deletePost(postId: string) {
     this.http.delete("http://localhost:3000/api/posts/" + postId)
-    .subscribe(() => {
-      const updatedPosts = this.posts.filter(post =>post.id !== postId);
-      this.posts = updatedPosts;
-      this.postsUpdated.next([...this.posts]);
-    })
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      })
   }
 }
