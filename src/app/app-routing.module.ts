4feb9f938441a1dd4629188aa8804f4252ff0573
tercette <leandro.tercette@gmail.com>
2022-10-09 14:29:49 -0300

    finishing the edit feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 const routes: Routes = [
-  { path: '', component:PostListComponent },
-  { path: 'create', component:PostCreateComponent }
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent },
+  { path: 'edit/:postId', component: PostCreateComponent }
 ];
 
 @NgModule({
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { map, Subject } from "rxjs";
 
 
 import { Post } from "./post.model";
+import { response } from "express";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
@@ -36,6 +37,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(p => p.id ===id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
@@ -46,6 +51,13 @@ export class PostsService {
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+    .put("http://localhost:3000/api/posts/" + id, post)
+    .subscribe(response => console.log(response))
   }
 
   deletePost(postId: string){
