4f0e90e3babda2262d7b0adb5db26b3d480affb1
tercette <leandro.tercette@gmail.com>
2022-10-08 22:11:21 -0300

    data stored in mongo sucessfully

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Post = require('./models/post')
+const mongoose = require("mongoose")
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Leandro:AimoEOtrVggGMssQ@cluster0.dr3klpv.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log("Connected to database!");
+})
+.catch(() => {
+  console.log('Connections failed!');
+})
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,8 +31,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
