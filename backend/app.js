f0d6cbf6fb0bd29e0eadc9c2a2d161ce1808ecac
tercette <leandro.tercette@gmail.com>
2022-10-09 22:40:19 -0300

    image saves in back end and retrieved

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -15,6 +16,7 @@ mongoose.connect("mongodb+srv://Leandro:AimoEOtrVggGMssQ@cluster0.dr3klpv.mongod
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
