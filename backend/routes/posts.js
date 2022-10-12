b9e0147879fe0ecbb982676c0e8dfd1c848dbc19
tercette <leandro.tercette@gmail.com>
2022-10-12 14:44:57 -0300

    fixed

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,4 +1,3 @@
-const { PreloadingStrategy } = require("@angular/router");
 const express = require("express");
 const multer = require("multer");
 
@@ -60,7 +59,7 @@ router.put(
     let imagePath = req.body.imagePath;
     if (req.file) {
       const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename
+      imagePath = url + "/images/" + req.file.filename;
     }
     const post = new Post({
       _id: req.body.id,
@@ -79,24 +78,22 @@ router.get("", (req, res, next) => {
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
-  let fechedPosts;
-  if(pageSize && currentPage) {
-    postQuery
-    .skip(pageSize * (currentPage -1))
-    .limit(pageSize);
+  let fetchedPosts;
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
   postQuery
-  .then(documents => {
-    fechedPosts = documents
-    return Post.count();
-  })
-  .then(count => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: fechedPosts
-      maxPost: count
+    .then(documents => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: fetchedPosts,
+        maxPosts: count
+      });
     });
-  });
 });
 
 router.get("/:id", (req, res, next) => {
