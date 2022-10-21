862283920ea7c9c632c0f15a9b9115e5f064b2d4
tercette <leandro.tercette@gmail.com>
2022-10-20 23:50:28 -0300

    using the user id in the front end

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private tokenTimer: any;
+  private userId: string;
   private authStatusListener = new Subject<boolean>()
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -21,6 +22,10 @@ export class AuthService {
 
   getIsAuth(){
     return this.isAuthenticated
+  }
+
+  getUserId() {
+    return this.userId;
   }
 
   getAuthStatusListener() {
@@ -37,7 +42,7 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post<{ token: string, expiresIn: number }>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{ token: string, expiresIn: number, userId: string }>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
@@ -45,10 +50,11 @@ export class AuthService {
           const expiresInDuration = response.expiresIn;
           this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
+          this.userId  =response.userId;
           this.authStatusListener.next(true);
           const now = new Date();
           const expirationDate = new Date( now.getTime() + expiresInDuration * 1000);
-          this.saveAuthData(token, expirationDate )
+          this.saveAuthData(token, expirationDate, this.userId )
           console.log(expirationDate)
           this.router.navigate(['/']);
         }
@@ -65,6 +71,7 @@ export class AuthService {
     if(expiresIn > 0) {
       this.token = authInformation.token;
       this.isAuthenticated = true;
+      this.userId = authInformation.userId;
       this.setAuthTimer(expiresIn/1000);
       this.authStatusListener.next(true)
     }
@@ -74,9 +81,10 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(['/']);
-    this.clearAuthData();
+    this.userId = null
     clearTimeout(this.tokenTimer)
+    this.clearAuthData();
+    this.router.navigate(['/']);
   }
 
   private setAuthTimer(duration: number) {
@@ -86,25 +94,30 @@ export class AuthService {
     }, duration * 1000)
   }
 
-  private saveAuthData(token: string, expirationDate: Date){
+  private saveAuthData(token: string, expirationDate: Date, userId: string){
     localStorage.setItem('token', token);
     localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem("userId", userId)
   }
 
   private clearAuthData(){
     localStorage.removeItem("token");
     localStorage.removeItem("expiration");
+    localStorage.removeItem("userId");
+
   }
 
   private getAuthData(){
     const token = localStorage.getItem("token");
     const expirationDate = localStorage.getItem("expiration");
+    const userId = localStorage.getItem("userId")
     if(!token || !expirationDate) {
       return;
     }
     return {
       token: token,
-      expirationDate: new Date(expirationDate)
+      expirationDate: new Date(expirationDate),
+      userId: userId
     }
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,7 +23,8 @@ export class PostListComponent implements OnInit, OnDestroy {
   postsPerPage = 5;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
-  userIsAuthenticated = false
+  userIsAuthenticated = false;
+  userId: string;
   private postsSub: Subscription;
   private authStatusSub: Subscription
 
@@ -36,6 +37,7 @@ export class PostListComponent implements OnInit, OnDestroy {
     ngOnInit() {
       this.isLoading = true;
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
+      this.userId = this.authService.getUserId();
       this.postsSub = this.postsService
         .getPostUpdateListener()
         .subscribe((postData: {posts: Post[], postCount: number}) => {
@@ -48,6 +50,7 @@ export class PostListComponent implements OnInit, OnDestroy {
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
         this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
       });
     }
 
